0fbaa5e485305f5ac3ff31950b245a72b219079b
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-10-01 21:52:02 +0600

    fix: add exception for body content

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,33 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use AlAminFirdows\LaravelEditorJs\Facades\LaravelEditorJs;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
-class Post extends Model
+final class Post extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'blocks'];
 
-    public function getBodyAttribute()
+    public function getBodyAttribute(): ?string
     {
-        return LaravelEditorJs::render($this->blocks);
+        try {
+            return LaravelEditorJs::render($this->blocks);
+        } catch (Exception $e) {
+            return '';
+        }
     }
 
-    public function getExcerptAttribute()
+    public function getExcerptAttribute(): ?string
     {
-        return substr(strip_tags($this->body), 0, 120);
+        return substr(strip_tags($this->body), 0, 120) ?? '';
     }
 
-    public function getPublishedAtAttribute()
+    public function getPublishedAtAttribute(): string
     {
         return $this->created_at->diffForHumans();
     }
 
-    public function scopeLatest($query)
+    public function scopeLatest($query): Builder
     {
         return $query->orderBy('created_at', 'desc');
     }
