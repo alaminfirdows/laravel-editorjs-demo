21bcc4af8a9a7256ae46bb554714c1ae3dec3970
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-10-01 22:03:06 +0600

    fix: update route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/posts');
 
 Route::resource('posts', PostController::class);
