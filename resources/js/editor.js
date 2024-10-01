e223fa87b5d2008200f8781eb977aced95d39bb3
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-10-01 21:47:04 +0600

    feat: add core tools

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -1,4 +1,12 @@
 import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import LinkTool from "@editorjs/link";
+import Quote from "@editorjs/quote";
+import RawTool from "@editorjs/raw";
+import SimpleImage from "@editorjs/simple-image";
+import Checklist from "@editorjs/checklist";
+import Embed from "@editorjs/embed";
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const data = JSON.parse(document.getElementById("blocks").value || "{}");
@@ -6,10 +14,56 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const editor = new EditorJS({
         holder: "editorjs",
 
-        onChange: async function () {
-            const data = await editor.save();
+        tools: {
+            // header
+            header: Header,
 
-            document.getElementById("blocks").value = JSON.stringify(data);
+            // list
+            list: {
+                class: List,
+                inlineToolbar: true,
+                config: {
+                    defaultStyle: "unordered",
+                },
+            },
+
+            // link
+            linkTool: {
+                class: LinkTool,
+                config: {
+                    endpoint: "http://localhost:8000/fetch-url", // Your backend endpoint for url data fetching,
+                },
+            },
+
+            // quote
+            quote: Quote,
+
+            // raw
+            raw: RawTool,
+
+            // image
+            image: SimpleImage,
+
+            // checklist
+            checklist: {
+                class: Checklist,
+                inlineToolbar: true,
+            },
+
+            // embed
+            embed: Embed,
+        },
+
+        onChange: function () {
+            editor
+                .save()
+                .then((data) => {
+                    document.getElementById("blocks").value =
+                        JSON.stringify(data);
+                })
+                .catch((error) => {
+                    console.error("Saving failed: ", error);
+                });
         },
 
         data: data,
